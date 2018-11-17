a0aebfe61759575fc814c36635eda3f22750924f
janmuixi <janmuixi7@gmail.com>
2018-11-17 21:31:57 +0100

    Global chat finished, need to make a private chat

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -2,11 +2,34 @@ $(function () {
 
     const socket = io();
 
-    // getting elements from DOM
+    // getting elements from DOM / interface
     const $messageForm = $('#message-form');
     const $messageBox = $('#message');
     const $chat = $('#chat');
 
+    // getting elements from DOM / nickname
+    const $nickForm = $('#nickForm');
+    const $nickName = $('#nickName');
+    const $nickError = $('#nickError');
+
+    const $users = $("#usernames");
+
+    $nickForm.submit (e => {
+        e.preventDefault();
+        socket.emit('new user', $nickName.val(), data => {
+            if (data) {
+                $('#nickWrap').hide();
+                $('#contentWrap').show();
+            } else {
+                $nickError.html(`
+                    <div class="alert alert-danger">
+                        That username already exists
+                    </div>
+                `);
+            }
+            $nickName.val('');
+        })
+    });
     // events
     $messageForm.submit ( e => {
         e.preventDefault();
@@ -16,6 +39,14 @@ $(function () {
     })
 
     socket.on('new message', function (data) {
-        $chat.append(data + '<br/>')
+        $chat.append('<b>' + data.nick + '</b> ' + data.msg + '<br/>');
     })
+
+    socket.on('usernames', data => {
+        let html = '';
+        for (let i = 0; i < data.length; i++) {
+            html += `<p><i class="fas fa-user"></i> ${data[i]}</p>`
+        }
+        $users.html(html);
+    });
 });
